2a55d7a656f87b00aea6e649c99f90a17b217b13
pvarouktsis <p.varouktsis@gmail.com>
2021-01-21 19:17:43 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+#A miniPython example
+
+
+def minipythonfunct(a, b=2, d):
+    return (5 + 3.2)
+
+if not (x) < (3+2) or p < 5:
+    while true:
+        if false:
+            print 5
+
+
+    for index in papaki:
+        while (x + 5) == index:
+            j -= i
+
+    print (3 + 2) * 3 * id['boo'] + max(5, 5)
+
+    j = funct(j+5)
+
+    file = open('assignment asdf ASDAf', index)
+
+    print type(file)
+
+    print [arg1, arg2, arg3]
+
+    assert (c + 1), 'string'
+
+    myfunct(y, x, 5)
+
+    p['string' + 1] = 'value'
+
+    none = 'None'
+
+    o = id.push()
